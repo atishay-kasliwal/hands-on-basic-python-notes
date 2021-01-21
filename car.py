d916dc44c48ec085626ff8c6ae06a65b8972ed1a
Atishay Kasliwal <61780293+atishay-kasliwal@users.noreply.github.com>
2021-01-21 20:52:20 +0530

    Add files via upload

---
+++ car.py
@@ -0,0 +1,13 @@
+"""Represent gas and electric cars."""
+
+class Car():
+    """A simple attempt to model a car."""
+  
+ 
+class Battery():
+    """A battery for an electric car."""
+   
+    
+class ElectricCar(Car):
+    """A simple model of an electric car."""
+    
